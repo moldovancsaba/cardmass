28500aa70c8a347f782bf11426f0a620a0274dfa
Frame It Now Developer <moldovancsaba@gmail.com>
2025-09-17 15:51:33 +0200

    feat(proof): new cards created on /proof default to #backlog (decline)

--- app/proof/page.tsx
+++ app/proof/page.tsx
@@ -4,7 +4,7 @@ export default function ProofPage() {
   return (
     <main className="p-4 bg-white text-black flex flex-col xl:h-screen xl:overflow-hidden">
       <div className="flex-1 xl:overflow-hidden">
-<Board initialView="matrix" axisHidden={true} titleOverrides={{ do: '#persona', decide: '#proposal', delegate: '#journey', decline: '#backlog' }} createDefaultStatus={'decide'} />
+<Board initialView="matrix" axisHidden={true} titleOverrides={{ do: '#persona', decide: '#proposal', delegate: '#journey', decline: '#backlog' }} createDefaultStatus={'decline'} />
       </div>
     </main>
   )
